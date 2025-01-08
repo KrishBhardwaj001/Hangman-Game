0d14230ef97ecda82b303fc55650fcc661eea626
Krish Bhardwaj <132665459+KrishBhardwaj001@users.noreply.github.com>
2025-01-08 20:31:49 +0530

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -1,38 +1,27 @@
+from words import random_words
 import random
+from art import logo, stages
 
-from word import word_list
+lives = len(stages) - 1
+chosen_word = random.choice(random_words) # random.choice() picks a random item from a list
+print(f"Chosen word: {chosen_word}")
 
-wordlist = ['london', 'camel', 'baboon']
-chosen_word = random.choice(wordlist)
-
-lives = 6
-guessed = set()
-
-from art import logo
 print(logo)
 
-display = ["_" for letter in chosen_word]
+blanks = ["_" for _ in chosen_word] # Create a list of underscores based on the length of the chosen_word
+print(" ".join(blanks))
 
-while '_' in display and lives > 0:
-    guess = input('Guess a letter: ').lower()
-    
-    if guess in guessed:
-        print('You have already guessed it. Try again!')
-        continue
-    guessed.add(guess)
-    
-    for index, letter in enumerate(chosen_word):
-        if letter == guess:
-            display[index] = guess
-    if guess not in chosen_word:
+while "_" in blanks and lives != 0:
+    guessed_letter = input("\nGuess a letter: ").lower()
+    if guessed_letter in chosen_word:
+        # Loop through the chosen_word and check if the guessed_letter is in the chosen_word
+        for index, letter in enumerate(chosen_word):
+            if letter == guessed_letter:
+                blanks[index] = guessed_letter # Replace the underscore with the guessed_letter if it's correct
+    else:
+        print(stages[lives])
         lives -= 1
-    print(display)
-    print(f"{lives} lives are left")  
-    
-if '_' not in display:
-    print('You won!')
-else:
-    print('You lost!')  
+    print(" ".join(blanks))
 
-from art import stages
-print(stages[lives])
+if "".join(blanks) == chosen_word:
+    print("\nYou win!")

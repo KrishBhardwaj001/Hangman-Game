d71e5605ea838e8ea68ef247518b7bb1d5c7bc7c
Krish Bhardwaj <132665459+KrishBhardwaj001@users.noreply.github.com>
2025-01-08 20:46:14 +0530

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -4,8 +4,6 @@ from art import logo, stages
 
 lives = len(stages) - 1
 chosen_word = random.choice(random_words) # random.choice() picks a random item from a list
-print(f"Chosen word: {chosen_word}")
-
 print(logo)
 
 blanks = ["_" for _ in chosen_word] # Create a list of underscores based on the length of the chosen_word
@@ -14,10 +12,13 @@ print(" ".join(blanks))
 while "_" in blanks and lives != 0:
     guessed_letter = input("\nGuess a letter: ").lower()
     if guessed_letter in chosen_word:
-        # Loop through the chosen_word and check if the guessed_letter is in the chosen_word
-        for index, letter in enumerate(chosen_word):
-            if letter == guessed_letter:
-                blanks[index] = guessed_letter # Replace the underscore with the guessed_letter if it's correct
+        if guessed_letter in blanks:
+            print(f"\nYou've already guessed {guessed_letter}")
+        else:
+            # Loop through the chosen_word and check if the guessed_letter is in the chosen_word
+            for index, letter in enumerate(chosen_word):
+                if letter == guessed_letter:
+                    blanks[index] = guessed_letter # Replace the underscore with the guessed_letter if it's correct
     else:
         print(stages[lives])
         lives -= 1

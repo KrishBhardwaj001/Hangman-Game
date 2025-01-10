5225bc427547eed26d6c7f46dd5fdb06d75170bc
Krish Bhardwaj <132665459+KrishBhardwaj001@users.noreply.github.com>
2025-01-10 15:35:46 +0530

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -9,10 +9,10 @@ print(logo)
 blanks = ["_" for _ in chosen_word] # Create a list of underscores based on the length of the chosen_word
 print(" ".join(blanks))
 
-while "_" in blanks and lives != 0:
+while "_" in blanks and lives >= 0:
     guessed_letter = input("\nGuess a letter: ").lower()
     if guessed_letter in chosen_word:
-        if guessed_letter in blanks:
+        if guessed_letter in blanks and guessed_letter in chosen_word:
             print(f"\nYou've already guessed {guessed_letter}")
         else:
             # Loop through the chosen_word and check if the guessed_letter is in the chosen_word
@@ -20,9 +20,13 @@ while "_" in blanks and lives != 0:
                 if letter == guessed_letter:
                     blanks[index] = guessed_letter # Replace the underscore with the guessed_letter if it's correct
     else:
+        print("\nOOPS! Wrong Guess")
         print(stages[lives])
         lives -= 1
+        
     print(" ".join(blanks))
 
-if "".join(blanks) == chosen_word:
+if lives >= 0 and "_" not in blanks:
     print("\nYou win!")
+else:
+    print("\nYou lose!")
